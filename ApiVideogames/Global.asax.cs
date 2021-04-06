30b07a85bb76ea1970ff12916dcfb42d8af81849
jfcode27 <francojeremy23@gmail.com>
2021-04-05 22:31:52 -0500

    First Push

---
+++ ApiVideogames/Global.asax.cs
@@ -0,0 +1,15 @@
+using System.Web.Http;
+
+
+namespace ApiVideogames
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+
+        }
+    }
+}
