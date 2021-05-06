f44cced9adfbab1828760d50a09ecfcdb64e8233
jfcode27 <francojeremy23@gmail.com>
2021-05-06 00:17:35 -0500

    2Controllers

---
+++ ApiVideogames/Controllers/VideogamesCatalogueController.cs
@@ -0,0 +1,21 @@
+﻿using ApiVideogames.OutputAdapter.Repositories;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace ApiVideogames.Controllers
+{
+    [RoutePrefix("api/videogames")]
+    [EnableCors("*", "*", "*")]
+    public class VideogamesCatalogueController : ApiController
+    {
+        [Route("date")]
+        public HttpResponseMessage getVideogamesByDate()
+        {
+            var videogamesList = VideogamesRepository.ConsultarVideojuegosPorFecha();
+
+            return Request.CreateResponse(HttpStatusCode.OK, videogamesList);
+        }
+    }
+}
